b692e2b2e4bc1e8afec042076ae03bf4bdcd5ca4
Nick Nobles <Nicholas_Nobles@intuit.com>
2020-10-22 16:00:41 -0700

    Update ChannelWatchManager.py

--- utils/ChannelWatchManager.py
+++ utils/ChannelWatchManager.py
@@ -152,7 +152,7 @@ class ChannelManager:
         ajaxHelper = AternosAPIHelper.get_ajax_token_and_cookie("https://aternos.org/panel/ajax/account/login.php")
         print(ajaxHelper.token)
         print(ajaxHelper.headers)
-        resp = requests.post(url=f"https://aternos.org/panel/ajax/account/login.php?SEC={ajaxHelper.token}", data=arguments, headers=ajaxHelper.headers)
+        resp = requests.post(url=f"https://aternos.org/panel/ajax/account/login.php?SEC={ajaxHelper.token}", data=arguments, headers=ajaxHelper.headers, cookies=ajaxHelper.cookies)
         #print(arguments)
         #header = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36", "Cookie": "ATERNOS_SEC_0zc2e78xbgo00000=nytbpw1at5p00000; __cfduid=d530f2a278f10bc9772809555ae71a7151603394544; _ga=GA1.2.465278585.1603394547; _gid=GA1.2.2026183107.1603394547; PHPSESSID=3i3gea4lp65doc5odjhp1nt8k5; ATERNOS_STYLE=default; __gads=ID=e3b35e1d0c1f057b-22698d1942b800b0:T=1603394653:S=ALNI_MaOvMGphYHildKpP6wBNNG14xEbRg; id5id.1st_364_nb=1; cto_bidid=WdrDIl9qZUh4QlRGOEtxVHJRRFJXanAlMkJFcURjUTdEaUFzcXZkbHdyVlE1TDVQJTJGV0J2c1M3N214ZlFUejZYSHdvSnhFODQycHdvV0hBVVhhcCUyQmdkTkZyQWhDdyUzRCUzRA; cto_bundle=NHIpz196YURDc0ZzUmQzVTFON2RNRU5ZUlQwcnBEeUQ5UUNsYkY0MWRIJTJCTWZ1YjBSUlNjYiUyQlAyJTJCMXpVTFN6RjJrd1Z6bkVDam9meHA1VnpCS293OUtnU1hGOERFalliNWRQTmFKZFFUc3JhYThyQjVJOUJ4b1c4SXFaZEVrQTU5ME1XTQ" }
         #resp = requests.post(url=f"https://aternos.org/panel/ajax/account/login.php?SEC=0zc2e78xbgo00000:nytbpw1at5p00000", data=arguments, headers=header)
@@ -162,10 +162,16 @@ class ChannelManager:
 
 class AternosAPIHelper:
     class AjaxHelper:
-        def __init__(self, token, cookie):
-            self.token = token
-            self.cookie = cookie
-            self.headers = { "Cookie" : cookie }
+        def __init__(self, key, value, url):
+            COOKIE_PREFIX = "ATERNOS"
+            cookie_key = COOKIE_PREFIX + "_SEC_" + key 
+            cookie_value = value + ";path=" + url
+            self.token = key + ":" + value
+            self.cookies = { cookie_key: cookie_value }
+            self.headers = { "Cookie": cookie_key + "=" + cookie_value }
+
+    def __init__(self, url):
+        self.a = url
 
     @staticmethod
     def get_random_str(length):
@@ -180,7 +186,7 @@ class AternosAPIHelper:
         value = AternosAPIHelper.get_random_str(16)
         token = key + ":" + value
         cookie = COOKIE_PREFIX + "_SEC_" + key + "=" + value + ";path=" + url
-        return AternosAPIHelper.AjaxHelper(token, cookie)
+        return AternosAPIHelper.AjaxHelper(key, value, url)
 
 
 class Channel:
